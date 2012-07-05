cc3cbac8d10d1fb18d3ae7536d0d70516b72f376
William P. Riley-Land <william.riley.land@gmail.com>
2012-07-05 12:29:38 -0500

    flesh out the calls a bit more

--- index.js
+++ index.js
@@ -4,96 +4,115 @@ var mongoose = require('mongoose');
 var rest = {};
 var BASE_URI = '/api/';
 
-var get = function(model) {
+var model = function(schema) {
+    return mongoose.models[schema.statics.metadata.singular];
+};
+
+var get = function(schema) {
     // return the spcefied resource
     var r = function(request, response, next) {
-	console.log(model);
-
-	var id    = request.params.id;
-	var query = mongoose.models[model.statics.metadata.singular].findOne({ _id: id });
-
-	query.run( function (err, o) {
+	var id = request.params.id;
+	model(schema).findById(id).run( function (err, doc) {
 	    if (err) return next(err);
-	    response.json(o);
+	    response.json(o); // TODO return?
 	});
     };
 
     return r;
 };
 
-var post = function(model) {
+var post = function(schema) {
     // treat the given resource as a collection, and push the given object to it
 };
 
-var put = function (model) { 
+var put = function (schema) { 
     // replace given object, or create it if nonexistant
     var r = function(request, response, next) {
-	var id = request.params.id;
-	// update
+	var id = request.params.id || null; // TODO test this
+	var o = new schema(request.payload); // TODO
+	o.save();
     };
     
     return r;
 };
 
-var del = function (model) {
+var del = function (schema) {
     // delete the addressed object
     var r = function (request, response, next) {
 	var id = request.params.id;
-	// delete
+	model(schema).remove({ _id: id }, function (err, foo) {
+	    if (err) return next(err);
+	    console.log(foo);
+	    response.json(true);
+	});
     };
     
     return r;
 };
 
-var multiGet = function (model) {
+var multiGet = function (schema) {
     // TODO take range params, etc.
     var r = function (request, response, next) {
-	mongoose.models[model.statics.metadata.singular].find().run( function(err, os) {
+	var query = request.query || {}; // TODO validate?
+	model(schema).find(query).run( function(err, os) {
 	    if (err) next(err);
-	    response.json(os);
+	    response.json(os); // TODO return?
 	});
     };
 
     return r;
 };
 
-var multiPost = function (model) {
+var multiPost = function (schema) {
     // create a new object
-    // return it's ID or whole object
+    // return it's ID
     var r = function(request, response, next) {
-	var o = new model(request.payload); // TODO or something
-	model.save(o, function (err) {
+	var o = new schema(request.payload); // TODO or something
+	o.save(function (err, o) {
 	    if (err) next (err);
+	    response.json(o._id);
 	});
     };
 
     return r;
 };
 
-var multiPut = function (model) {
+var multiPut = function (schema) {
     // replace entire collection with given new collection
+    model(schema).remove({}, function (err, foo) {
+	if (err) return next(err);
+	console.log(foo);
+	// TODO return ?
+
+	// TODO add passed in docs
+    });
 };
 
-var multiDel = function (model) {
+var multiDel = function (schema) {
     // delete entire collection  
+    model(schema).remove({}, function (err, foo) {
+	if (err) return next(err);
+	console.log(foo);
+	// TODO return ?
+    });
 };
 
 express.HTTPServer.prototype.rest =
-express.HTTPSServer.prototype.rest = function (model) {
-    var metadata   = model.statics.metadata;
+express.HTTPSServer.prototype.rest = function (schema) {
+    var metadata   = schema.statics.metadata;
     var middleware = metadata.middleware || []; // TODO
     var singular   = BASE_URI + metadata.singular;
-    var plural     = BASE_URI + (metadata.plural || model.singular + 's');
+    var plural     = BASE_URI + (metadata.plural || schema.singular + 's');
 
-    this.get(singular + '/:id', middleware, get(model));
-    this.post(singular,         middleware, post(model));
-    this.put(singular + '/:id', middleware, put(model));
-    this.del(singular + '/:id', middleware, del(model));
+    this.get(singular + '/:id', middleware, get(schema));
+    this.post(singular,         middleware, post(schema));
+    this.put(singular + '/:id', middleware, put(schema));
+    this.del(singular + '/:id', middleware, del(schema));
 
-    this.get(plural,  middleware, multiGet(model));
-    this.post(plural, middleware, multiPost(model));
-    this.put(plural,  middleware, multiPut(model));
-    this.del(plural,  middleware, multiPut(model));
+    this.get(plural,  middleware, multiGet(schema));
+    this.post(plural, middleware, multiPost(schema));
+    this.put(plural,  middleware, multiPut(schema));
+    this.del(plural,  middleware, multiPut(schema));
 };
 
 // TODO could also modify mongoose.Schema with
