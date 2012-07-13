289756ef25248e657b926b7fd2fed58c85dbf7ff
William P. Riley-Land <william.riley.land@gmail.com>
2012-07-13 12:27:41 -0500

    working middleware tests and switch to mikeal's request module

--- test/middleware.js
+++ test/middleware.js
@@ -4,15 +4,15 @@ var mongoose      = require('mongoose');
 var express       = require('express');
 var passport      = require('passport')
 var LocalStrategy = require('passport-local').Strategy;
+var request       = require('request');
 
-var request  = require('./lib/request');
 var fixtures = requireindex('./test/fixtures');
 
 describe('Middleware', function () {
   before(function (done) { // TODO break these out into separate functions and get them to run sequentially (e.g. makeDb, make vege, make app, etc.)
         var Schema = mongoose.Schema;
 
-    mongoose.connect('mongodb://localhost/xXxBaUcIsTeStXxX'); // TODO probably check don't overwrite it ? ....
+    mongoose.connect('mongodb://localhost/xXxBaUcIsTeStXxX');
 
     var Vegetable = new Schema({
       name: String
@@ -38,6 +38,7 @@ describe('Middleware', function () {
       app.use(express.cookieParser());
       app.use(express.bodyParser());
       app.use(express.session({ secret: 'bass raccoon' }));
+      app.use(express.methodOverride());
       app.use(passport.initialize());
       app.use(passport.session());
     });
@@ -58,7 +59,22 @@ describe('Middleware', function () {
       }
     ));
 
-    app.post('/api/login', passport.authenticate('local'));
+    app.post('/api/login', function(request, response, next) {
+      passport.authenticate('local', function(err) {
+	if (err) return next(err);
+
+	var username = request.body.username;
+
+	if (username !== 'ok') return response.send(401);
+
+	var user = { name: username };
+
+	request.logIn(user, function(err) {
+	  if (err) return next(err);
+	  response.send(200);
+	});
+      })(request, response, next);
+    });
 
     app.rest(Vegetable);
     app.listen(8012);
@@ -70,41 +86,65 @@ describe('Middleware', function () {
   describe('passport and middleware', function () {
 
     it('should prevent resource from being loaded when not logged in', function (done) {
-      
-      request('GET', '/api/vegetable/' + vegetables[0]._id, function (err, r) {
+      var options = {
+	url: 'http://localhost:8012/api/vegetable/' + vegetables[0]._id,
+	json:true
+      }; 
+      request.get(options, function (err, response, body) {
 	if (err) return done(err);
-	expect(r.response.statusCode).to.be(401);
+	expect(response.statusCode).to.be(401);
 	done();
       });
     });
 
     it('should prevent resource to be loaded with wrong credentials', function (done) {
-      request('POST', '/api/login', { username: 'dude' }, function (err,r) {
-	if (err) return done(err);
-	expect(r.response.statusCode).to.be(401);
+      var options = {
+	url: 'http://localhost:8012/api/login',
+	json: {
+	  username: 'dude'
+	}
+      }; 
+      request.post(options, function (err, response, body) {
+    	if (err) return done(err);
+    	expect(response.statusCode).to.be(401);
 
-	request('GET', '/api/vegetable/' + vegetables[0]._id, function (err, r) {
-	  if (err) return done(err);
-	  expect(r.response.statusCode).to.be(401);
-	  done();
-	});
+	var options = {
+	  url: 'http://localhost:8012/api/vegetable/' + vegetables[0]._id,
+	  json: true
+	}; 
+
+    	request.get(options, function (err, response, body) {
+    	  if (err) return done(err);
+    	  expect(response.statusCode).to.be(401);
+    	  done();
+    	});
       });
     });
 
     it('should allow resource to be loaded with correct credentials', function (done) {
-      request('POST', '/api/login', { username: 'ok' }, function (err,r) {
-	if (err) return done(err);
-	expect(r.response.statusCode).to.be(200);
+      var options = {
+	url: 'http://localhost:8012/api/login',
+	json: {
+	  username: 'ok'
+	}
+      }; 
+      request.post(options, function (err, response, body) {
+    	if (err) return done(err);
+    	expect(response.statusCode).to.be(200);
 
-	request('GET', '/api/vegetable/' + vegetables[0]._id, function (err, r) {
-	  if (err) return done(err);
+	var options = {
+	  url: 'http://localhost:8012/api/vegetable/' + vegetables[0]._id,
+	  json: true
+	}; 
+
+    	request.get(options, function (err, response, body) {
+    	  if (err) return done(err);
+
+    	  expect(response.statusCode).to.be(200);
+    	  expect(body).to.have.property('name', 'Turnip');
 	  
-	  var doc = JSON.parse(r.body);
-	  expect(r.response.statusCode).to.be(200);
-	  expect(doc).to.have.property('name', 'Turnip');
-	  
-	  done();
-	});
+    	  done();
+    	});
       });
     });
 
