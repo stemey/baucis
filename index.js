4d78d0886deb4f89f9e8800b698f504109ed9c26
William P. Riley-Land <william.riley.land@gmail.com>
2012-07-12 21:49:25 -0500

    reindent to 2-spaces and fix git repo url

--- index.js
+++ index.js
@@ -5,169 +5,169 @@ var rest = {};
 var BASE_URI = '/api/'; // TODO config
 
 var model = function(schema) {
-    return mongoose.models[schema.metadata.singular];
+  return mongoose.models[schema.metadata.singular];
 };
 
 var get = function(schema) {
-    // return the spcefied resource
-    var r = function(request, response, next) {
-	var id = request.params.id;
-	model(schema).findById(id).run( function (err, doc) {
-	    if (err) return done(err); // TODO if id is not valid ObjectID then what? 500 is OK?
-	    if (doc === null) return response.send(404);
-	    response.json(doc); // TODO return?
-	});
-    };
-
-    return r;
+  // return the spcefied resource
+  var r = function(request, response, next) {
+    var id = request.params.id;
+    model(schema).findById(id).run(function (err, doc) {
+      if (err) return done(err); // TODO if id is not valid ObjectID then what? 500 is OK?
+      if (doc === null) return response.send(404);
+      response.json(doc); // TODO return?
+    });
+  };
+  
+  return r;
 };
 
 var post = function(schema) {
-    // treat the given resource as a collection, and push the given object to it
-    var r = function (request, response, next) {
-	// TODO should check if certain metadata is set and perform accordingly
-	response.send(405); // method not allowed (as of yet unimplemented)	 ???
-    };
-
-    return r;
+  // treat the given resource as a collection, and push the given object to it
+  var r = function (request, response, next) {
+    // TODO should check if certain metadata is set and perform accordingly
+    response.send(405); // method not allowed (as of yet unimplemented)	 ???
+  };
+  
+  return r;
 };
 
 var put = function (schema) { 
-    // replace given object, or create it if nonexistant
-    var r = function(request, response, next) {
-	var id  = request.params.id || null;
-
-	model(schema).update({_id: id}, request.body, {upsert: true}, function (err, doc) {
-	    if (err) return next(err);
-	    response.send(200);
-	});
-    };
+  // replace given object, or create it if nonexistant
+  var r = function(request, response, next) {
+    var id  = request.params.id || null;
     
-    return r;
+    model(schema).update({_id: id}, request.body, {upsert: true}, function (err, doc) {
+      if (err) return next(err);
+	    response.send(200);
+    });
+  };
+  
+  return r;
 };
 
 var del = function (schema) {
-    // delete the addressed object
-    var r = function (request, response, next) {
+  // delete the addressed object
+  var r = function (request, response, next) {
 	var id = request.params.id;
-	model(schema).remove({ _id: id }).run( function (err, foo) {
-	    if (err) return next(err);
-	    response.send(200); // TODO return num deleted?
-	});
-    };
+    model(schema).remove({ _id: id }).run( function (err, foo) {
+      if (err) return next(err);
+      response.send(200); // TODO return num deleted?
+    });
+  };
     
-    return r;
+  return r;
 };
 
 var multiGet = function (schema) {
-    // TODO take range params, etc.
-    var r = function (request, response, next) {
-	var query = {};//request.query || {}; // TODO validate? // get from JSON or queryStr
-	model(schema).find(query, function(err, docs) {
-	    if (err) return next(err);
-	    response.json(docs); // TODO return?
-	});
-    };
+  // TODO take range params, etc.
+  var r = function (request, response, next) {
+    var query = {};//request.query || {}; // TODO validate? // get from JSON or queryStr
+    model(schema).find(query, function(err, docs) {
+      if (err) return next(err);
+      response.json(docs); // TODO return?
+    });
+  };
 
-    return r;
+  return r;
 };
 
 var multiPost = function (schema) {
-    // create a new object and return its ID
-    var r = function(request, response, next) {
-	var o = new (model(schema))(request.body);
-	o.save(function (err, doc) {
-	    if (err) return next(err); // TODO catch dupe ID here and -> status code?
-	    response.json(doc._id);
-	});
-    };
-
-    return r;
+  // create a new object and return its ID
+  var r = function(request, response, next) {
+    var o = new (model(schema))(request.body);
+    o.save(function (err, doc) {
+      if (err) return next(err); // TODO catch dupe ID here and -> status code?
+      response.json(doc._id);
+    });
+  };
+  
+  return r;
 };
 
 var multiPut = function (schema) {
-    // replace entire collection with given new collection and return IDs
-    var r = function(request, response, next) {
-	model(schema).remove({}, function (err, foo) {
-	    if (err) return next(err);
-	    
-	    // TODO make sure is array (or marshal) and respond accordingly
-	    var docs = request.body
-	    var ids = [];
-	    var numSaved = 0;
-
-	    docs.forEach( function (data, i) { // TODO use some async lib here
-		var doc = new (model(schema))(data);
-
-		doc.save( function (err, doc) {
-		    if (err) return next(err);
-		    ids[i] = doc._id;
-		    numSaved++;
-		    if(numSaved === docs.length) response.json(ids);
-		});
-	    });
+  // replace entire collection with given new collection and return IDs
+  var r = function(request, response, next) {
+    model(schema).remove({}, function (err, foo) {
+      if (err) return next(err);
+      
+      // TODO make sure is array (or marshal) and respond accordingly
+      var docs = request.body
+      var ids = [];
+      var numSaved = 0;
+      
+      docs.forEach( function (data, i) { // TODO use some async lib here
+	var doc = new (model(schema))(data);
+	
+	doc.save( function (err, doc) {
+	  if (err) return next(err);
+	  ids[i] = doc._id;
+	  numSaved++;
+	  if(numSaved === docs.length) response.json(ids);
 	});
-    };
+      });
+    });
+  };
 
-    return r;
+  return r;
 };
 
 var multiDel = function (schema) {
-    // delete entire collection  
-    var r = function(request, response, next) {
-	model(schema).remove({}, function (err, count) {
-	    if (err) return next(err);
-	    response.json(count);
-	});
-    };
+  // delete entire collection  
+  var r = function(request, response, next) {
+    model(schema).remove({}, function (err, count) {
+      if (err) return next(err);
+      response.json(count);
+    });
+  };
 
-    return r;
+  return r;
 };
 
 express.HTTPServer.prototype.rest =
 express.HTTPSServer.prototype.rest = function (scheme) {
-    var schema;
-
-    if (scheme === 'Array') {
-	schema = scheme;
+  var schema;
+  
+  if (scheme === 'Array') {
+    schema = scheme;
+  }
+  else if (scheme === 'Object') {
+    schema = [];
+    for (key in scheme) {
+      if (!scheme.hasOwnProperty(key)) continue;
+      schema.push(scheme[key]);
     }
-    else if (scheme === 'Object') {
-	schema = [];
-	for (key in scheme) {
-	    if (!scheme.hasOwnProperty(key)) continue;
-	    schema.push(scheme[key]);
-	}
+  }
+  else {
+    schema = [scheme];
+  }
+
+  var that = this; // TODO
+  
+  schema.forEach( function (scheme) { // TODO name reuse :(
+    var metadata   = scheme.metadata;
+    var middleware = metadata.middleware || []; // TODO
+    var singular   = BASE_URI + metadata.singular;
+    var plural     = BASE_URI + (metadata.plural || metadata.singular + 's');
+    
+    // add if not already present
+    if (!model(scheme)) {
+      mongoose.model(scheme.metadata.singular, scheme, scheme.metadata.plural);
     }
-    else {
-	schema = [scheme];
-    }
-
-    var that = this; // TODO
-
-    schema.forEach( function (scheme) { // TODO name reuse :(
-	var metadata   = scheme.metadata;
-	var middleware = metadata.middleware || []; // TODO
-	var singular   = BASE_URI + metadata.singular;
-	var plural     = BASE_URI + (metadata.plural || metadata.singular + 's');
-
-	// add if not already present
-	if (!model(scheme)) {
-	    mongoose.model(scheme.metadata.singular, scheme, scheme.metadata.plural);
-	}
-	
-	that.get(singular + '/:id', middleware, get(scheme));
-	that.post(singular,         middleware, post(scheme));
-	that.put(singular + '/:id', middleware, put(scheme));
-	that.del(singular + '/:id', middleware, del(scheme));
-	
-	that.get(plural,  middleware, multiGet(scheme));
-	that.post(plural, middleware, multiPost(scheme));
-	that.put(plural,  middleware, multiPut(scheme));
-	that.del(plural,  middleware, multiDel(scheme));
-    });
+    
+    that.get(singular + '/:id', middleware, get(scheme));
+    that.post(singular,         middleware, post(scheme));
+    that.put(singular + '/:id', middleware, put(scheme));
+    that.del(singular + '/:id', middleware, del(scheme));
+    
+    that.get(plural,  middleware, multiGet(scheme));
+    that.post(plural, middleware, multiPost(scheme));
+    that.put(plural,  middleware, multiPut(scheme));
+    that.del(plural,  middleware, multiDel(scheme));
+  });
 };
 		    
 mongoose.Schema.prototype.metadata = function (data) { // TODO make this cool and test
-    this.metadata = data;
+  this.metadata = data;
 };
 		    
--- test/test.js
+++ test/test.js
@@ -10,24 +10,23 @@ var Schema = mongoose.Schema;
 mongoose.connect('mongodb://localhost/xXxBaUcIsTeStXxX'); // TODO probably check don't overwrite it ? ....
 
 var Vegetable = new Schema({
-    name: String
+  name: String
 },
 {
-    safe: true,
-    strict: true
+  safe: true,
+  strict: true
 });
 
 Vegetable.metadata({
-    singular: 'vegetable',
-    plural: 'vegetables'
+  singular: 'vegetable',
+  plural: 'vegetables'
 });
 
 mongoose.model(Vegetable.metadata.singular, Vegetable, Vegetable.metadata.plural);
 
 var app = express.createServer();
 app.configure(function(){
-    app.use(express.bodyParser());
-    //app.use(express.errorHandler()); 
+  app.use(express.bodyParser());
 });
 
 app.rest(Vegetable);
@@ -38,278 +37,278 @@ var vegetables;
 // ---------------------------------
 
 var request = function (method, path, data, callback) {
-    if (callback === undefined) {
-	callback = data;
-	data = undefined;
-    }
+  if (callback === undefined) {
+    callback = data;
+    data = undefined;
+  }
 
-    var options = {
-	hostname: '127.0.0.1',
-	method: method,
-	path: path,
-	port: 8012,
-	headers: {
-	    'Content-Type': 'application/json'
-	}
+  var options = {
+    hostname: '127.0.0.1',
+    method: method,
+    path: path,
+    port: 8012,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+    
+  var request = http.request(options, function (response) {
+    var r = {
+      body: '',
+      response: response
     };
     
-    var request = http.request(options, function (response) {
-	var r = {
-	    body: '',
-	    response: response
-	};
-
-	response.setEncoding('utf8');
-
-	response.on('error', function (err) {
-	    callback(err);
-	});
-	response.on('data', function (chunk) {
-	    r.body += chunk;
-	});
-	response.on('end', function () {
-	    callback(null, r);
-	});
-    });
+    response.setEncoding('utf8');
     
-    if (data !== undefined) request.write(JSON.stringify(data)); // TODO check if buffer or object or string
-    request.end();
+    response.on('error', function (err) {
+      callback(err);
+    });
+    response.on('data', function (chunk) {
+      r.body += chunk;
+    });
+    response.on('end', function () {
+      callback(null, r);
+    });
+  });
+  
+  if (data !== undefined) request.write(JSON.stringify(data)); // TODO check if buffer or object or string
+  request.end();
 };
 
 describe('REST web services', function () {
-    beforeEach(function(done) {
-	// clear all first
-	var vegetable = mongoose.models['vegetable'];
-
-	vegetable.remove({}, function (err) {
-	    if (err) return done(err)
-
-	    var names = ['Turnip', 'Spinach', 'Pea', 'Shitake', '', '', '', 'Radicchio'];
-	    vegetables = names.map( function (name) {
-		return new vegetable({ name: name });
-	    });
-
-	    var numberToSave = names.length;
-	    
-	    vegetables.forEach( function (vege) {
-		vege.save( function (err) {
-		    if (err) return done(err);
-		    numberToSave--;
-		    if( numberToSave === 0 ) done();
-		});
-	    });
+  beforeEach(function(done) {
+    // clear all first
+    var vegetable = mongoose.models['vegetable'];
+    
+    vegetable.remove({}, function (err) {
+      if (err) return done(err)
+      
+      var names = ['Turnip', 'Spinach', 'Pea', 'Shitake', '', '', '', 'Radicchio'];
+      vegetables = names.map( function (name) {
+	return new vegetable({ name: name });
+      });
+      
+      var numberToSave = names.length;
+      
+      vegetables.forEach( function (vege) {
+	vege.save( function (err) {
+	  if (err) return done(err);
+	  numberToSave--;
+	  if( numberToSave === 0 ) done();
 	});
+      });
     });
-
-    // TODO knowck down / build up
-    describe('GET singular', function () {
-	it('should get the addressed document', function(done){
-	    var turnip = vegetables[0];
-	    request('GET', '/api/vegetable/' + turnip._id, function (err, r) {
-		if (err) return done(err);
-
-		var doc = JSON.parse(r.body);
-		expect(doc._id).to.be(turnip._id.toString());
-		expect(doc).to.have.property('name', 'Turnip');
-		done();
-	    });
-	});
-
-	it('should return a good error', function (done) {
-	    request('GET', '/api/vegetable/666666666666666666666666', function (err, r) {
-		if (err) return done(err);
-
-		var response = r.response;
-		expect(response.statusCode).to.be(404);
-		done();
-	    }); 
-	});
-    });
-
-    describe('POST singular', function () {
-	it('should treat the given resource as a collection, and push the given object to it', function (done) {
-	    done(new Error('TODO: unimplemented'));
-	});
-    });
-
-    describe('PUT singular', function () {
-	it("should replace the addressed object if it exists", function (done) {
-	    var data = {
-		name: 'Leek'
-	    };
-
-	    var radicchio = vegetables[7];
-
-	    // first, check it's not the leek
-	    request('GET', '/api/vegetable/' + radicchio._id, function (err, r) {
-		if (err) return done(err);
-
-		var doc = JSON.parse(r.body);
-
-		expect(r.response.statusCode).to.be(200);
-		expect(doc).to.have.property('name', 'Radicchio');
-
-		// put the leek on the server
-		request('PUT', '/api/vegetable/' + radicchio._id, data, function (err, r) {
-		    if (err) return done(err);
-// TODO return id? //		    expect(id).to.be(7);
-
-		    // check it's not Radicchio
-		    request('GET', '/api/vegetable/' + radicchio._id, function (err, r) {
-			if (err) return done(err);
-
-			var doc = JSON.parse(r.body);
-			expect(doc).to.have.property('name', 'Leek');
-			done();
-		    });
-		});
-	    });
-	});
+  });
+  
+  // TODO knowck down / build up
+  describe('GET singular', function () {
+    it('should get the addressed document', function(done){
+      var turnip = vegetables[0];
+      request('GET', '/api/vegetable/' + turnip._id, function (err, r) {
+	if (err) return done(err);
 	
-	it('should create the addressed document if non-existant', function (done) {
-	    var data = {
-		name: 'Cucumber'
-	    };
-
-	    var id = 'badbadbadbadbadbadbadbad';
-
-	    // first check it's not there
-	    request('GET', '/api/vegetable/' + id, function (err, r) {
-		if (err) return done(err);
-		expect(r.response.statusCode).to.be(404);
-
-		// put it on server
-		request('PUT', '/api/vegetable/' + id, data, function (err, r) {
-		    if (err) return done(err);
-
-		    expect(r.response.statusCode).to.be(200);
-
-		    // check it's there
-		    request('GET', '/api/vegetable/' + id, function (err, r) {
-			if (err) return done(err);
-
-			var doc = JSON.parse(r.body);
-			expect(doc).to.have.property('_id', id);
-			expect(doc).to.have.property('name', 'Cucumber');
-			done();
-		    });
-		});
-	    });
-
-	});
+	var doc = JSON.parse(r.body);
+	expect(doc._id).to.be(turnip._id.toString());
+	expect(doc).to.have.property('name', 'Turnip');
+	done();
+      });
     });
-
-    describe('DEL singular', function () {
-	it('should delete the addressed document', function (done) {
-	    		    
-	    // make sure it's there
-	    var shitake = vegetables[3];
-	    var url     = '/api/vegetable/' + shitake._id;
-
-	    request('GET', url, function (err, r) {
-		if (err) return done(err);
-
-		var doc = JSON.parse(r.body);
-		expect(doc).to.have.property('name', 'Shitake');
-		
-		request('DELETE', url, function (err, r) {
-		    if (err) return done(err);
-
-		    expect(r.response.statusCode).to.be(200);
-
-		    request('GET', url, function (err, r) {
-			if (err) return done(err);
-
-			expect(r.response.statusCode).to.be(404);
-			done();
-		    });
-		});
-	    });
-
-	});
+    
+    it('should return a good error', function (done) {
+      request('GET', '/api/vegetable/666666666666666666666666', function (err, r) {
+	if (err) return done(err);
+	
+	var response = r.response;
+	expect(response.statusCode).to.be(404);
+	done();
+      }); 
     });
-
-    describe('GET plural', function () {
-	it("should return 'em all", function (done) {
-	    request('GET', '/api/vegetables/', function (err, r) {
-		if (err) return done(err);
-
- 		var docs = JSON.parse(r.body);
-		expect(r.response.statusCode).to.be(200);
-		docs.forEach( function(doc, i) {
-		    var vege = vegetables[i];
-		    expect(doc._id).to.be(vege._id.toString());
-		    expect(doc.name).to.be(vege.name);
-		});
-		done();
-	    });
-	});
+  });
+  
+  describe('POST singular', function () {
+    it('should treat the given resource as a collection, and push the given object to it', function (done) {
+      done(new Error('TODO: unimplemented'));
     });
-
-    describe('POST plural', function () {
-	it('should create a new object and return its ID', function (done) {
-
-	   var data = {
-	       name: 'Turnip'
-	   };
-
-	   request('POST', '/api/vegetables/', data, function (err, r) {
-	       if (err) return done(err);
-
-	       var id = JSON.parse(r.body);
-	       expect(id).to.not.be.empty();
-
-	       request('GET', '/api/vegetable/' + id, function (err, r) {
-		   if (err) return done(err);
-		   
-		   expect(r.response.statusCode).to.be(200);
- 
-		   var doc = JSON.parse(r.body);
-		   expect(doc).to.have.property('name', 'Turnip');
-
-		   done();
-	       });
-	   });
+  });
+  
+  describe('PUT singular', function () {
+    it("should replace the addressed object if it exists", function (done) {
+      var data = {
+	name: 'Leek'
+      };
+      
+      var radicchio = vegetables[7];
+      
+      // first, check it's not the leek
+      request('GET', '/api/vegetable/' + radicchio._id, function (err, r) {
+	if (err) return done(err);
+	
+	var doc = JSON.parse(r.body);
+	
+	expect(r.response.statusCode).to.be(200);
+	expect(doc).to.have.property('name', 'Radicchio');
+	
+	// put the leek on the server
+	request('PUT', '/api/vegetable/' + radicchio._id, data, function (err, r) {
+	  if (err) return done(err);
+	  // TODO return id? //		    expect(id).to.be(7);
+	  
+	  // check it's not Radicchio
+	  request('GET', '/api/vegetable/' + radicchio._id, function (err, r) {
+	    if (err) return done(err);
+	    
+	    var doc = JSON.parse(r.body);
+	    expect(doc).to.have.property('name', 'Leek');
+	    done();
+	  });
 	});
-
+      });
     });
-
-    describe('PUT plural', function () {
-	it('should replace entire collection with given new collection', function (done) {
-	    var poke = {
-		name: 'Poke'
-	    };
-	    var collards = {
-		name: 'Collard Greens'
-	    };
-	    var mustard = {
-		name: 'Mustard'
-	    };
-	    var newVegetables = [ poke, collards, mustard ];
-
-	    // TODO should first check current state is expected
-	    request('PUT', '/api/vegetables/', newVegetables, function (err, r) {
-		if (err) return done(err);
-
-		var ids = JSON.parse(r.body);
-		expect(r.response.statusCode).to.be(200);
-		expect(ids).to.have.property('length', 3); // TODO more?
-		done();
-	    });
+    
+    it('should create the addressed document if non-existant', function (done) {
+      var data = {
+	name: 'Cucumber'
+      };
+      
+      var id = 'badbadbadbadbadbadbadbad';
+      
+      // first check it's not there
+      request('GET', '/api/vegetable/' + id, function (err, r) {
+	if (err) return done(err);
+	expect(r.response.statusCode).to.be(404);
+	
+	// put it on server
+	request('PUT', '/api/vegetable/' + id, data, function (err, r) {
+	  if (err) return done(err);
+	  
+	  expect(r.response.statusCode).to.be(200);
+	  
+	  // check it's there
+	  request('GET', '/api/vegetable/' + id, function (err, r) {
+	    if (err) return done(err);
+	    
+	    var doc = JSON.parse(r.body);
+	    expect(doc).to.have.property('_id', id);
+	    expect(doc).to.have.property('name', 'Cucumber');
+	    done();
+	  });
 	});
+      });
+      
     });
+  });
 
-    describe('DEL plural', function () {
-	it('should delete all documents in addressed collection', function (done) {
-	    request('DELETE', '/api/vegetables/', function (err, r) {
-		if (err) return done(err);
-
-		var count = JSON.parse(r.body);
-		expect(r.response.statusCode).to.be(200);
-		expect(count).to.be(8);
-		done();
-	    });
+  describe('DEL singular', function () {
+    it('should delete the addressed document', function (done) {
+      
+      // make sure it's there
+      var shitake = vegetables[3];
+      var url     = '/api/vegetable/' + shitake._id;
+      
+      request('GET', url, function (err, r) {
+	if (err) return done(err);
+	
+	var doc = JSON.parse(r.body);
+	expect(doc).to.have.property('name', 'Shitake');
+	
+	request('DELETE', url, function (err, r) {
+	  if (err) return done(err);
+	  
+	  expect(r.response.statusCode).to.be(200);
+	  
+	  request('GET', url, function (err, r) {
+	    if (err) return done(err);
+	    
+	    expect(r.response.statusCode).to.be(404);
+	    done();
+	  });
 	});
+      });
+      
     });
-
+  });
+  
+  describe('GET plural', function () {
+    it("should return 'em all", function (done) {
+      request('GET', '/api/vegetables/', function (err, r) {
+	if (err) return done(err);
+	
+ 	var docs = JSON.parse(r.body);
+	expect(r.response.statusCode).to.be(200);
+	docs.forEach( function(doc, i) {
+	  var vege = vegetables[i];
+	  expect(doc._id).to.be(vege._id.toString());
+	  expect(doc.name).to.be(vege.name);
+	});
+	done();
+      });
+    });
+  });
+  
+  describe('POST plural', function () {
+    it('should create a new object and return its ID', function (done) {
+      
+      var data = {
+	name: 'Turnip'
+      };
+      
+      request('POST', '/api/vegetables/', data, function (err, r) {
+	if (err) return done(err);
+	
+	var id = JSON.parse(r.body);
+	expect(id).to.not.be.empty();
+	
+	request('GET', '/api/vegetable/' + id, function (err, r) {
+	  if (err) return done(err);
+	  
+	  expect(r.response.statusCode).to.be(200);
+	  
+	  var doc = JSON.parse(r.body);
+	  expect(doc).to.have.property('name', 'Turnip');
+	  
+	  done();
+	});
+      });
+    });
+    
+  });
+  
+  describe('PUT plural', function () {
+    it('should replace entire collection with given new collection', function (done) {
+      var poke = {
+	name: 'Poke'
+      };
+      var collards = {
+	name: 'Collard Greens'
+      };
+      var mustard = {
+	name: 'Mustard'
+      };
+      var newVegetables = [ poke, collards, mustard ];
+      
+      // TODO should first check current state is expected
+      request('PUT', '/api/vegetables/', newVegetables, function (err, r) {
+	if (err) return done(err);
+	
+	var ids = JSON.parse(r.body);
+	expect(r.response.statusCode).to.be(200);
+	expect(ids).to.have.property('length', 3); // TODO more?
+	done();
+      });
+    });
+  });
+  
+  describe('DEL plural', function () {
+    it('should delete all documents in addressed collection', function (done) {
+      request('DELETE', '/api/vegetables/', function (err, r) {
+	if (err) return done(err);
+	
+	var count = JSON.parse(r.body);
+	expect(r.response.statusCode).to.be(200);
+	expect(count).to.be(8);
+	done();
+      });
+    });
+  });
+  
 });
