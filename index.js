5f069be835ca52605e53cf943daef065ecbefdd1
William P. Riley-Land <william.riley.land@gmail.com>
2012-09-26 16:26:31 -0500

    Update for Express 3 (probably breaks tests)

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var lingo    = require('lingo');
 var BASE_URI = '/api/'; // TODO config
 
 var model = function(schema) {
-  return mongoose.models[schema.metadata('singular')];
+  return mongoose.model(schema.metadata('singular'));
 };
 
 var get = function(schema) {
@@ -25,7 +25,7 @@ var get = function(schema) {
 var post = function(schema) {
   // treat the addressed document as a collection, and push the addressed object to it (?)
   var r = function (request, response, next) {
-    response.send(405); // method not allowed (as of yet unimplemented)	 ???
+    response.send(405); // method not allowed (as of yet unimplemented)	 ??? //TODO or 501?
   };
   
   return r;
@@ -38,7 +38,7 @@ var put = function (schema) {
     
     model(schema).update({_id: id}, request.body, {upsert: true}, function (err, doc) {
       if (err) return next(err);
-      response.send(200);
+      response.send(200); // TODO send 201 when creating or 200/204 when updating
     });
   };
   
@@ -75,15 +75,19 @@ var pluralGet = function (schema) {
 var pluralPost = function (schema) {
   // create a new document and return its ID
   var r = function(request, response, next) {
-    var given = request.body || [];
+    if (!request.body || request.body.length === 0) {
+      return next(new Error('Must supply a document or array to POST'));
+    }
 
+    var ids   = [];
+    var given = request.body;
     if (!Array.isArray(given)) given = [given];
 
     var docs = given.map( function(e) { 
       return new (model(schema))(e);
     });
 
-    var ids = [];
+    response.statusCode = 201;
 
     docs.forEach( function (doc) {
       doc.save(function (err, doc) {
@@ -100,7 +104,7 @@ var pluralPost = function (schema) {
 var pluralPut = function (schema) {
   // repalce all docs with given docs ... 
   var r = function (request, response, next) {
-    response.send(405); // method not allowed (as of yet unimplemented)	 ???
+    response.send(405); // method not allowed (as of yet unimplemented)	 ??? // TODO 501?
   };
 
   return r;
@@ -120,8 +124,11 @@ var pluralDel = function (schema) {
   return r;
 };
 
-express.HTTPServer.prototype.rest =
-express.HTTPSServer.prototype.rest = function (schemata) {
+module.exports = {};
+
+// TODO maybe check if express.HTTPServer exists and hook that up for backward compat.
+
+module.exports.rest = function (app, schemata) {
   if (!Array.isArray(schemata)) {
     // if array leave alone, otherwise
     if (schemata.paths) { 
@@ -136,8 +143,6 @@ express.HTTPSServer.prototype.rest = function (schemata) {
     }
   }
 
-  var app = this;
-  
   schemata.forEach( function (schema) {
     var singular    = schema.metadata('singular');
     var plural      = schema.metadata('plural') || lingo.pluralize(singular);
@@ -150,11 +155,13 @@ express.HTTPSServer.prototype.rest = function (schemata) {
       mongoose.model(singular, schema, plural);
     }
 
+//    app.head(singularUrl, middleware, head(schema)); // TODO
     app.get(singularUrl,  middleware, get(schema));
     app.post(singularUrl, middleware, post(schema));
     app.put(singularUrl,  middleware, put(schema));
     app.del(singularUrl,  middleware, del(schema));
         
+//    app.head(pluralUrl, middleware, pluralHead(schema)); // TODO
     app.get(pluralUrl,  middleware, pluralGet(schema));
     app.post(pluralUrl, middleware, pluralPost(schema));
     app.put(pluralUrl,  middleware, pluralPut(schema));
