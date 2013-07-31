14522f87ef59b3f6e3e09a6e79534930ce595d2c
William Riley-Land <william@kun.io>
2013-07-31 00:14:11 -0500

    Fix issue where schema was not deep-copied

--- userMiddlewareSchema.js
+++ userMiddlewareSchema.js
@@ -1,23 +1,20 @@
-// __Dependencies__
-var extend = require('util')._extend;
-
-// __Private Members__
-var schema = {
-  "request":
-  { "instance": { "head": [], "get": [], "post": [], "put": [], "del": [] },
-   "collection": { "head": [], "get": [], "post": [], "put": [], "del": [] }
-  },
-  "query":
-  { "instance": { "head": [], "get": [], "post": [], "put": [], "del": [] },
-   "collection": { "head": [], "get": [], "post": [], "put": [], "del": [] }
-  },
-  "documents":
-  { "instance": { "head": [], "get": [], "post": [], "put": [], "del": [] },
-   "collection": { "head": [], "get": [], "post": [], "put": [], "del": [] }
-  }
-}
-
 // __Module Definition__
+
+// A function that returns a hash with an HTTP verb entry for each stage and "howMany"
 module.exports = function () {
-  return extend({}, schema);
+  var schema = {
+    "request":
+    { "instance": { "head": [], "get": [], "post": [], "put": [], "del": [] },
+     "collection": { "head": [], "get": [], "post": [], "put": [], "del": [] }
+    },
+    "query":
+    { "instance": { "head": [], "get": [], "post": [], "put": [], "del": [] },
+     "collection": { "head": [], "get": [], "post": [], "put": [], "del": [] }
+    },
+    "documents":
+    { "instance": { "head": [], "get": [], "post": [], "put": [], "del": [] },
+     "collection": { "head": [], "get": [], "post": [], "put": [], "del": [] }
+    }
+  };
+  return schema;
 };
